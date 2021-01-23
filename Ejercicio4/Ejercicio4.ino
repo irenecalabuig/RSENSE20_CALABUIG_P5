de9fdcb0b621195d1763325ee39efe43c42005b4
irenecalabuig <71815934+irenecalabuig@users.noreply.github.com>
2021-01-23 23:44:37 +0100

    4. Chat Bluetooth
    Establecer un “chat” Bluetooth con perfil SPP con una APP en el móvil:

---
+++ Ejercicio4/Ejercicio4.ino
@@ -0,0 +1,26 @@
+//----------------------------------------- PRÁCTICA 5 ------------------------------------------//
+// Ejercicio 4. Establece un “chat” Bluetooth con perfil SPP con una APP en el móvil: 
+
+#include "BluetoothSerial.h"
+// crea un puente entre Bluetooth serial y clásico (SPP)
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
+
+BluetoothSerial SerialBT;
+
+void setup() {
+  Serial.begin(115200);
+  SerialBT.begin("ESP32test"); //Bluetooth device name
+  Serial.println("The device started, now you can pair it with bluetooth!");
+}
+
+void loop() {
+  if (Serial.available()) {
+    SerialBT.write(Serial.read());
+  }
+  if (SerialBT.available()) {
+    Serial.write(SerialBT.read());
+  }
+  delay(20);
+}
